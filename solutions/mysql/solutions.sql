eefb79f504a42ee2bb8227f4dc1e9488c4e43b32
Epharas <cris0922@stud.kea.dk>
2019-04-08 10:48:56 +0200

    Solve ex. 6 - 10

--- solutions/mysql/solutions.sql
+++ solutions/mysql/solutions.sql
@@ -39,3 +39,39 @@ SELECT a.* FROM Album a
   INNER JOIN Genre g ON g.GenreId=t.GenreId
   WHERE g.name = 'Bossa Nova' AND t.Name LIKE 'Samba%'
   GROUP BY t.AlbumId;
+
+
+-- 6. For each genre, show the average length of its songs in minutes (without indicating seconds). Use the
+--    headers “Genre” and “Minutes”, and include only genres that have any song longer than half an hour.
+select g.Name Genre, AVG(t.Milliseconds) DIV 60000 Minutes FROM Genre g
+  INNER JOIN Track t on t.GenreId=g.GenreId
+  WHERE t.Milliseconds > 30 * 60000
+  GROUP BY g.GenreId;
+
+
+-- 7. How many client companies have no state?
+SELECT COUNT(1) FROM Customer WHERE Company IS NOT NULL AND State IS NULL;
+
+
+-- 8. For each employee with clients in the “USA”, “Canada” and “Mexico” show the number of clients from
+--    these countries s/he has given support, only when this number is higher than 6. Sort the query by
+--    number of clients. Regarding the employee, show his/her first name and surname separated by a
+--    space. Use “Employee” and “Clients” as headers.
+SELECT COUNT(c.Country) Clients, CONCAT(e.FirstName, ' ', e.LastName) Employee, c.Country FROM Employee e
+  INNER JOIN Customer c ON c.SupportRepId = e.EmployeeId
+  WHERE c.Country IN ('USA', 'Canada', 'Mexico')
+  GROUP BY c.Country HAVING clients > 6
+  ORDER BY Clients;
+
+
+-- 9. For each client from the “USA”, show his/her surname and name (concatenated and separated by a
+--    comma) and their fax number. If they do not have a fax number, show the text “S/he has no fax”. Sort
+--    by surname and first name.
+SELECT CONCAT(FirstName, ',', LastName) Customer, IFNULL(Fax, 'S/he has no fax') Fax FROM Customer
+  WHERE Country = 'USA' ORDER BY Customer;
+
+
+-- 10. For each employee, show his/her first name, last name, and their age at the time they were hired.
+
+-- This solution should account for leap years
+SELECT FirstName, LastName, TIMESTAMPDIFF(YEAR, BirthDate, HireDate) FROM Employee;
